84f3d5e89554b8f0c7a0a5c669194a89b1d20eef
Ismail Govande <ismailgovande14@gmail.com>
2026-09-24 16:09:25 +0530

    Updated server code

--- server.js
+++ server.js
@@ -8,3 +8,22 @@ const app = express();
 
 app.use(express.json());
 app.use(express.static("frontend"));
+
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => {
+        console.log("MongoDB connected");
+        app.listen(5000, () => {
+            console.log("Server running on port 5000");
+        });
+    })
+    .catch((err) => console.log(err));
+
+app.get("/employees", async (req, res) => {
+    const employees = await Employee.find();
+
+    res.json({
+        success: true,
+        count: employees.length,
+        data: employees
+    });
+});
